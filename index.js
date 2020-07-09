8b867aa50d1e3a625868c93376febcdae7497620
jasonelliots <j.elliotschwartz@gmail.com>
2020-07-09 09:48:45 -0700

    super server

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require("./api/server.js");
 
 const port = 5000;
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  console.log(`\n*** Super Server Running on http://localhost:${port} ***\n`);
 });
